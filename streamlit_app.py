ab31770dc8f6671069e73f30a6fbc122b02489dc
Jules <117381424+julesthelittleX@users.noreply.github.com>
2022-11-03 17:53:02 +0100

    Update streamlit_app.py
    new header and recipes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
-streamlit.title('My Parents new health diner')
-
 streamlit.header('Breakfast Menu')
+
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('avocado toast')
+
+streamlit.header('Build your own fruit smoothie')
